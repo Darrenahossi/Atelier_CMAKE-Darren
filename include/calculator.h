8aae02a901ff50e682934b1521754bff43f8fd09
Darrenahossi <77730071+Darrenahossi@users.noreply.github.com>
2024-05-13 11:55:54 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,10 +1,5 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-float divide(int a, int b);
-int square(int a);
-
-#endif /* CALCULATOR_H */
+double _add(double a, double b);
+double _sub(double a, double b);
+double _mul(double a, double b);
+double _div(double a, double b);
+double square(double x);
