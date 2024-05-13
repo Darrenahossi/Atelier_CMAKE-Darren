43f72c61bd11e1dc87b75e0dadde8a8fa7a63723
Darrenahossi <77730071+Darrenahossi@users.noreply.github.com>
2024-05-13 11:57:44 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
-#include "calculator.h"
+#include <stdlib.h>
+#include <string.h>
+#include "./../include/calculator.h"
 
-int main() {
-    int result = square(5);
-    printf("Square of 5 is: %d\n", result);
+int main(int argc, char argv[]) {
+    if (argc == 3) {
+        char op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else if (argc == 2 && strcmp(argv[1], "car") == 0) {
+        // Traiter le calcul du carré
+        double number = atof(argv[2]);
+        printf("Le carré de %lf est %lf\n", number, square(number));
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+
     return 0;
-}
-	
+}	
