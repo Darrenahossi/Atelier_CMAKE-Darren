38559abb84371b5fec42856ed48f71383475ec9c
Darrenahossi <77730071+Darrenahossi@users.noreply.github.com>
2024-05-13 12:16:43 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,42 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "./../include/calculator.h"
+#include "calculator.h"
 
-int main(int argc, char argv[]) {
-    if (argc == 3) {
-        char op = argv[1];
-        char* a = argv[2];
-        double r = 0;
+int main() {
+    int result = 0;
+    result = add(5, 3);
+    printf("Addition: %d\n", result);
 
-        if (strcmp(op, "add") == 0) {
-            r = _add(atof(a), atof(argv[3]));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "sub") == 0) {
-            r = _sub(atof(a), atof(argv[3]));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "mul") == 0) {
-            r = _mul(atof(a), atof(argv[3]));
-            printf("%lf", r);
-        }
-        else if (strcmp(op, "div") == 0) {
-            r = _div(atof(a), atof(argv[3]));
-            printf("%lf", r);
-        }
-        else {
-            printf("Erreur de parametres");
-        }
-    }
-    else if (argc == 2 && strcmp(argv[1], "car") == 0) {
-        // Traiter le calcul du carré
-        double number = atof(argv[2]);
-        printf("Le carré de %lf est %lf\n", number, square(number));
-    }
-    else {
-        printf("Erreur de parametres");
-    }
+    result = subtract(10, 7);
+    printf("Subtraction: %d\n", result);
+
+    result = multiply(4, 6);
+    printf("Multiplication: %d\n", result);
+
+    result = divide(20, 5);
+    printf("Division: %d\n", result);
+
+    result = square(5);
+    printf("Square: %d\n", result);
 
     return 0;
-}	
+}  
